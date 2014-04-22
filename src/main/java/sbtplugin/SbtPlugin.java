7dbc56cfcd76d248de079d5a960c6c48b55751bb
Marcelo Vanzin <vanza@users.sourceforge.net>
2014-04-21 19:47:10 -0700

    Automatically open dockable when active project uses sbt.

--- src/main/java/sbtplugin/SbtPlugin.java
+++ src/main/java/sbtplugin/SbtPlugin.java
@@ -13,26 +13,60 @@
 //  0. You just DO WHAT THE FUCK YOU WANT TO.
 package sbtplugin;
 
-import java.awt.EventQueue;
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
+import org.gjt.sp.jedit.EditBus;
+import org.gjt.sp.jedit.EditBus.EBHandler;
+import org.gjt.sp.jedit.EditPlugin;
 import org.gjt.sp.jedit.View;
 import org.gjt.sp.jedit.jEdit;
-import org.gjt.sp.jedit.EditBus;
-import org.gjt.sp.jedit.EditPlugin;
-import org.gjt.sp.jedit.msg.EditorExitRequested;
-import org.gjt.sp.jedit.msg.PluginUpdate;
 import org.gjt.sp.jedit.msg.ViewUpdate;
-import static org.gjt.sp.jedit.EditBus.EBHandler;
-
-import org.gjt.sp.util.Log;
+import projectviewer.ProjectViewer;
+import projectviewer.event.ViewerUpdate;
+import projectviewer.vpt.VPTProject;
 
 public class SbtPlugin extends EditPlugin {
+
+  @Override
+  public void start() {
+    EditBus.addToBus(this);
+    for (View v : jEdit.getViews()) {
+      checkDockable(v);
+    }
+  }
+
+  @Override
+  public void stop() {
+    EditBus.removeFromBus(this);
+  }
+
+  @EBHandler
+  public void projectViewerUpdate(ViewerUpdate update) {
+    if (update.getType() == ViewerUpdate.Type.PROJECT_LOADED) {
+      checkDockable(update.getView());
+    }
+  }
+
+  @EBHandler
+  public void viewUpdate(ViewUpdate update) {
+    if (update.getWhat() == ViewUpdate.CREATED) {
+      checkDockable(update.getView());
+    }
+  }
+
+  private void checkDockable(View view) {
+    VPTProject project = ProjectViewer.getActiveProject(view);
+    if (project == null) {
+      return;
+    }
+
+    Object console = view.getDockableWindowManager()
+        .getDockable("sbtconsole");
+    if (console != null) {
+      return;
+    }
+
+    if (SbtOptionsPane.getBoolean(project, SbtOptionsPane.SBT_ENABLED)) {
+      jEdit.getAction("sbtconsole").invoke(view);
+    }
+  }
 
 }
